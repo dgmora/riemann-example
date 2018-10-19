de212b7a224477111565eee7eb028a5cc13ddbb7
David Garcia <david.garcia@heyjobs.de>
2018-10-19 16:56:23 +0200

    Configure mettric

--- riemann_producer/lib/riemann_producer.rb
+++ riemann_producer/lib/riemann_producer.rb
@@ -4,9 +4,11 @@ require 'mettric'
 set :bind, '0.0.0.0'
 
 get '/' do
+  Mettric.config = { host: 'localhost', port: '5555', app: 'producer' }
+
   event = params[:event] || 'none'
   tag = params[:tag] || 'untagged'
   description = params[:description] || 'Not-desc'
-  🛎  service: "sinatra.producer.#{event}", tags: [tag], description: description
+  Mettric.event(service: "sinatra.producer.#{event}", tags: [tag], description: description)
   "*event processed* event: #{event}, tag #{tag}, description: #{description}"
 end
